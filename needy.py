869a2f5d5b8c505b2a56c965aa58b77a46567c71
absakash <akashboswas2@gmail.com>
2024-06-05 21:30:19 +0530

    first commit

---
+++ needy.py
@@ -0,0 +1,14 @@
+# pip install tk-tools
+# pip install opencv-contrib-python
+# pip install datetime
+# pip install pytest-shutil
+# pip install python-csv
+# pip install numpy
+# pip install pillow 
+# pip install pandas
+# pip install times
+
+
+#install of all the things before start ....
+
+# then go got the idle .. open it then open here the main file ,, then run module ,, done 
